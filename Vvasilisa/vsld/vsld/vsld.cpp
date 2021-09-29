1ddc8320abcc331bbbb6b13688db211b7c78148b
gy <vasilisa.kolchenko@ya.ru>
2021-09-29 13:01:14 +0300

    изменины символы для вывода YES

---
+++ Vvasilisa/vsld/vsld/vsld.cpp
@@ -0,0 +1,18 @@
+﻿// vsld.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c, x, y;
+    cin >> a >> b >> c;
+    cin >> x >> y;
+        if (((a = x) && (b = y)) || ((a = x) && (c = y)) || ((b = x) && (c = y))) {
+            cout << "YES";
+            }
+        else {
+            cout << "NO";
+            }
+}
+
---
+++ v5/v5/v5.cpp
@@ -0,0 +1,15 @@
+﻿// v5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, x, y;
+    cin >> a >> b >> c >> x >> y;
+    if ((a = x) && (b = y) || (())
+
+
+}
+
